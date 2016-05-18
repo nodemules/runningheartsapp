7e6aafe4125db97f92a91d60ce8a153da91e2e57
ProbableCohort <drscogin@gmail.com>
2016-05-18 00:35:00 -0400

    feature/RHP_31-implement_events_feature
    Add Game endpoints
    Update mongoose to v4.4.17
    Increment version to 0.0.1
    Use badass populate queries
    Fix broken ng-options track by
    Fix profile page

--- app/api/main.js
+++ app/api/main.js
@@ -39,7 +39,7 @@ api.post('/venues', function(req, res) {
 api.get('/venues/:venueId', function(req, res, next) {
     Venues.findById(req.params.venueId, function(err, venue) {
         if (err)
-            res.send(err);
+            console.log(err.stack);
         res.json(venue);
     });
 });
@@ -68,7 +68,6 @@ api.get('/tds', function(req, res, next) {
 //Add Modify Players =====
 //========================
 
-//TODO: need to setup a loop here to only return the usernames, otherwise we are sending their pass, usertype etc.
 api.get('/players', function(req, res, next) {
     Players
         .find()
@@ -117,9 +116,27 @@ api.put('/event', function(req, res) {
 })
 
 api.get('/event', function(req, res) {
+  var pOptions = [
+    { 
+      path : 'venue', 
+      select : 'name day' 
+    }, 
+    { 
+      path : 'td', 
+      select : 'name user', 
+      populate : { 
+        path : 'user', 
+        model : 'User', 
+        select : 'local.username' 
+      } 
+    },
+    {
+      path : 'games'
+    }
+  ];
   Event
     .find()
-    .populate('venue td')
+    .populate(pOptions)
     .exec(function(err, events) {
       if (err)
         res.send(err);
@@ -128,13 +145,135 @@ api.get('/event', function(req, res) {
 })
 
 api.get('/event/:id', function(req, res) {
+  var pOptions = [
+    { 
+      path : 'venue', 
+      select : 'name day' 
+    }, 
+    { 
+      path : 'td', 
+      select : 'name user', 
+      populate : { 
+        path : 'user', 
+        model : 'User', 
+        select : 'local.username' 
+      } 
+    },
+    {
+      path : 'games'
+    }
+  ];
   Event
     .findById(req.params.id)
-    .populate('venue td')
+    .populate(pOptions)
     .exec(function(err, events) {
       if (err)
         res.send(err);
       res.send(events);
+    })
+})
+
+// GAMES
+
+api.post('/game', function(req, res) {
+  Game.create(req.body, function(err, game) {
+    if (err)
+      console.log(err.stack);
+    Event.findOne({ _id : game['event'] }, function(err, e) {
+      if (err)
+        console.log(err.stack);
+      e.games.push(game._id);
+      e.save();
+      res.send(game);
+    })
+  })
+})
+
+api.put('/game', function(req, res) {
+  Game.findOneAndUpdate({ _id : req.body._id }, req.body, { "new" : true },
+    function (err, game) {
+      if (err)
+        res.send(err);
+      res.send(game);
+  })
+})
+
+api.get('/game', function(req, res) {
+  var pOptions = [
+    { 
+      path : 'event',
+      select : 'venue td date',
+      populate: [
+        {
+          path : 'venue', 
+          select : 'name day'
+        },
+        { 
+          path : 'td', 
+          select : 'name user', 
+          populate : { 
+            path : 'user', 
+            model : 'User', 
+            select : 'local.username' 
+          } 
+        }
+      ]
+    }
+  ];
+  Game
+    .find()
+    .populate(pOptions)
+    .exec(function(err, games) {
+      if (err)
+        res.send(err);
+      res.send(games);
+    })
+})
+
+api.get('/game/:id', function(req, res) {
+  var pOptions = [
+    { 
+      path : 'event',
+      populate: [
+        {
+          path : 'venue', 
+          select : 'name day'
+        },
+        { 
+          path : 'td', 
+          select : 'name user', 
+          populate : { 
+            path : 'user', 
+            model : 'User', 
+            select : 'local.username' 
+          } 
+        },
+        {
+          path : 'games',
+          select : 'event number'
+        }
+      ]
+    },
+    {
+      path : 'players',
+      populate : {
+        path : 'player', 
+        select : 'name user', 
+        populate : { 
+          path : 'user', 
+          model : 'User', 
+          select : 'local.username' 
+        } 
+      }
+    }
+  ];
+  Game
+    .findById(req.params.id)
+    .populate(pOptions)
+    .exec(function(err, games) {
+      if (err)
+        res.send(err);
+      res.send(games);
     })
 })
 
