e9b9a3a803d8053470ffefed0881d7ee706ccddd
staticsound <johngreedjr@gmail.com>
2016-06-05 22:37:39 -0400

    feature/RHP_22-refactor_api_to_be_restful
    Add usersController for api.

--- app/api/main.js
+++ app/api/main.js
@@ -4,6 +4,7 @@ var express = require('express'),
 var venuesController  = require('./venuesController'),
     eventsController  = require('./eventsController'),
     gamesController   = require('./gamesController'),
+    usersController   = require('./usersController'),
     playersController = require('./playersController');
 
 
@@ -15,36 +16,8 @@ api.use(function(req, res, next) {
 api.use('/venues', venuesController);
 api.use('/events', eventsController);
 api.use('/games', gamesController);
-api.use('/players', playersController)
+api.use('/players', playersController);
+api.use('/users', usersController);
 
-var Venues  = require('../models/venue'),
-    Users   = require('../models/user'),
-    Players = require('../models/player'),
-    Event   = require('../models/event'),
-    Game    = require('../models/game');
-
-//TODO: need to setup a loop here to only return the usernames, otherwise we are sending their pass, usertype etc.
-api.get('/tds', function(req, res, next) {
-  Users
-    .find({usertype: 2})
-    .populate('player')
-    .exec(function(err, tds) {
-        if (err)
-            res.send(err);
-        res.json(tds);
-    });
-});
-
-//========================
-//Add Modify Players =====
-//========================
-
-api.put('/user', function(req, res){
-    Users.findOneAndUpdate({_id: req.body._id}, req.body, function (err, user) {
-    if (err)
-        res.send(err);
-    res.send(user);
-    });
-});
 
 module.exports = api;
---
+++ app/api/usersController.js
@@ -0,0 +1,65 @@
+var express = require('express'),
+    api     = express.Router();
+
+var Venues  = require('../models/venue'),
+    Users   = require('../models/user'),
+    Players = require('../models/player'),
+    Event   = require('../models/event'),
+    Game    = require('../models/game');
+
+api.get('/', function(req, res) {
+  Users
+    .find()
+    .select('local.username usertype')
+    .exec(function(err, user){
+      if (err)
+        console.log(err.stack);
+      res.send(user);
+    })
+});
+
+api.get('/:id', function(req, res) {
+  Users
+    .findById(req.params.id)
+    .select('local.username usertype')
+    .exec(function(err, user){
+      if (err)
+        console.log(err.stack);
+      res.send(user);
+    })
+});
+
+api.get('/type/:typeId', function(req, res, next) {
+  Users
+    .find({usertype: req.params.typeId})
+    .select('local.username usertype player')
+    .populate('player')
+    .exec(function(err, users) {
+        if (err)
+            res.send(err);
+        res.json(users);
+    });
+});
+
+api.put('/', function(req, res){
+  Users
+    .findOneAndUpdate({_id: req.body._id}, req.body, { "new" : true }) 
+    .exec(function (err, user) {
+      if (err)
+          res.send(err);
+      res.send(user);
+    });
+});
+
+api.delete('/:id', function(req, res) {
+  Users
+    .findByIdAndRemove(req.params.id)
+    .exec(function (err) {
+      if (err)
+        console.log(err.stack);
+      res.send();
+    })
+});
+
+
+module.exports = api;
