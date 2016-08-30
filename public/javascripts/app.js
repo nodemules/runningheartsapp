f6d2d014b933b6f1bbb66e06fb76cd695b2ee101
Brent Haertlein <brenthaertlein@gmail.com>
2016-08-29 23:41:55 -0400

    feature/RHP_50-create-seasonal-standings-api-and-view
    Add stats routing & ctrl
    Add stats.player routing & ctrl
    Add statsService
    Retrieve info from /api/stats/players

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -162,6 +162,22 @@ var APP_NAME = 'runningHeartsApp';
           controllerAs: 'gp'
         })
 
+      $stateProvider
+        .state('stats', {
+          url : 'stats',
+          parent: 'home',
+          templateUrl: '/views/stats.html',
+          controller: 'statsCtrl',
+          controllerAs: 'vm'
+        })
+        .state('stats.players', {
+          url : '/players',
+          parent: 'stats',
+          templateUrl: '/views/stats.players.html',
+          controller: 'statsPlayersCtrl',
+          controllerAs: 'vm'
+        })
+
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
---
+++ public/javascripts/services/statsService.js
@@ -0,0 +1,34 @@
+// global angular
+(function(angular) {
+  angular.module(APP_NAME).factory('statsService', statsService);
+
+  statsService.$inject = [ '$resource' ];
+
+  function statsService($resource) {
+    var basePath = '/api/stats';
+
+    var service = {
+      api : api
+    }
+
+    return service;
+    
+    /////////////////////
+    
+    function api(id) {
+      return $resource(basePath + '/:action/:id', {
+        id : id
+      }, {
+        'players' : {
+          method : 'get',
+          params : {
+            action : 'players'
+          },
+          isArray : true
+        }
+      });
+    }
+
+  }
+
+})(angular);
