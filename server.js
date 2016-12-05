b23d4014af94ba56d6be7960ce14d1ea90cddf56
staticsound <johngreedjr@gmail.com>
2016-12-04 21:21:03 -0500

    feature/RHP_60-implement-users-v2 setup passport

--- server.js
+++ server.js
@@ -2,17 +2,19 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var path     = require('path');
-var app      = express();
-var port     = process.env.RHP_PORT || 8080;
-var mongoose = require('mongoose');
-var flash    = require('connect-flash');
+var express    = require('express');
+var path       = require('path');
+var app        = express();
+var port       = process.env.RHP_PORT || 8080;
+var mongoose   = require('mongoose');
+var morgan     = require('morgan');
+var bodyParser = require('body-parser');
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var session       = require('express-session');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local'), Strategy;
+var flash         = require('connect-flash');
+var cookieParser  = require('cookie-parser');
 
 var api = require('./app/api/main');
 var configDB = require('./config/database.js');
@@ -22,9 +24,19 @@ mongoose.connect(configDB.url); // connect to our database
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//authentication
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(flash());
+app.use(session({
+  secret: 'wouldyoupleasestopshiningthatthinginmyeyesnow',
+  saveUnitialized: true,
+  resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static(path.join(__dirname, 'public')));
