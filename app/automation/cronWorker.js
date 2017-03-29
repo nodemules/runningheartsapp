09b296085c37860bddb87df399eb571208b21e45
staticsound <johngreedjr@gmail.com>
2017-03-28 21:41:45 -0400

    feature/RHP_94-autocreate-events Nightly cron instead of dev every minute

--- app/automation/cronWorker.js
+++ app/automation/cronWorker.js
@@ -4,7 +4,7 @@
     var Automator = require('./Automator')();
 
     //Dev: running every minute
-    new CronJob('0 * * * * *', function() {
+    new CronJob('* * 0 * * *', function() {
       Automator.createNewEvents();
 
     }, null, true, 'America/New_York');
