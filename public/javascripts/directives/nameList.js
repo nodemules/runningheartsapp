b5690ef53e0c1482777f1c1783af2b1cb2e16894
ProbableCohort <brenthaertlein@gmail.com>
2017-08-12 20:09:21 -0400

    feature/RHP_202-update-venues-list
    directive for the stupid list of names
    convert venues to new list style

---
+++ public/javascripts/directives/nameList.js
@@ -0,0 +1,22 @@
+{
+  /* global angular, APP_NAME */
+
+  angular.module(APP_NAME).directive('rhpNameList', rhpNameList);
+
+  rhpNameList.$inject = [];
+
+  function rhpNameList() {
+
+    var directive = {
+      restrict: 'E',
+      templateUrl: '../views/templates/list/nameList.html',
+      scope: {
+        list: '=ngModel'
+      }
+    }
+
+    return directive;
+
+  }
+
+}
