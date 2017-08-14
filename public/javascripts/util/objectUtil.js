6ed85c527f315efe6f971e5da8d07692d17247b6
ProbableCohort <brenthaertlein@gmail.com>
2017-08-14 01:36:29 -0400

    feature/RHP_202-update-venues-list
    getting a little carried away here

--- public/javascripts/util/objectUtil.js
+++ public/javascripts/util/objectUtil.js
@@ -7,17 +7,30 @@
 
   function objectUtil() {
 
+    const OBJECT_OBJECT = '[object Object]';
+    const OBJECT_TYPEOF = 'object';
+
     var service = {
       setProperty
     };
 
     return service;
 
-    function setProperty(obj, path, value) {
+    function isObject(obj) {
+      var byCall = Object.prototype.toString.call(obj) === OBJECT_OBJECT;
+      var byInstance = (obj instanceof Object);
+      var byType = (typeof obj === OBJECT_TYPEOF);
+      return byCall && byInstance && byType;
+    }
+
+    function setProperty(path, value, obj) {
+      if (!isObject(obj)) {
+        obj = {};
+      }
       var parts = path.split('.'),
         part;
       while (part = parts.shift()) {
-        if (typeof obj[part] != 'object') {
+        if (!isObject(obj[part])) {
           obj[part] = {};
         }
         if (parts.length === 0) {
@@ -26,6 +39,7 @@
           obj = obj[part];
         }
       }
+      return obj;
     }
   }
 
--- public/javascripts/util/utils.js
+++ public/javascripts/util/utils.js
@@ -4,22 +4,38 @@
   utils.$inject = ['arrayUtil', 'objectUtil'];
 
   function utils(arrayUtil, objectUtil) {
+
+    const MESSAGE_REQUIRE_ARRAY = 'This method requires an Array as the first argument.';
+    const MESSAGE_REQUIRE_OBJECT = 'This method requires an Object as the first argument.';
+
     var service = {
       arrays,
-      objects
+      objects,
+      strings
     };
 
     return service;
 
     function arrays(arr) {
+      if (!arr || !Array.isArray(arr)) {
+        console.error(MESSAGE_REQUIRE_ARRAY)
+        return {};
+      }
       return {
-        findAll: (value, props) => arrayUtil.findAll(arr, value, props)
+        findOne: (value) => arrayUtil.findOne(arr, value),
+        findAll: (value, props, exact) => arrayUtil.findAll(arr, value, props, exact)
       }
     }
 
     function objects(obj) {
       return {
-        setProperty: (path, value) => objectUtil.setProperty(obj, path, value)
+        setProperty: (path, value) => objectUtil.setProperty(path, value, obj)
+      }
+    }
+
+    function strings(str) {
+      return {
+        isString: () => stringUtil.isString(str)
       }
     }
 
