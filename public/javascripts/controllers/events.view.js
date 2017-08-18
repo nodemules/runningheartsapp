e4c975c4f0fe1243d12c2404205ce58f347d6e56
ProbableCohort <brenthaertlein@gmail.com>
2017-08-17 23:42:24 -0400

    feature/RHP_203-update-events-list
    delete event button on event view

--- public/javascripts/controllers/events.view.js
+++ public/javascripts/controllers/events.view.js
@@ -1,13 +1,12 @@
-// global angular
-(function(angular) {
+{
 
-  'use strict';
+  /* global angular, APP_NAME */
 
   angular.module(APP_NAME).controller('eventsViewCtrl', eventsViewCtrl);
 
-  eventsViewCtrl.$inject = ['$filter', '$state', '$stateParams', 'eventsService', 'playersService', 'venuesService', 'gamesService', 'dialogService', 'errorService'];
+  eventsViewCtrl.$inject = ['$state', '$stateParams', 'eventsService', 'gamesService', 'dialogService', 'errorService'];
 
-  function eventsViewCtrl($filter, $state, $stateParams, eventsService, playersService, venuesService, gamesService, dialogService, errorService) {
+  function eventsViewCtrl($state, $stateParams, eventsService, gamesService, dialogService, errorService) {
 
     var vm = this;
 
@@ -15,19 +14,21 @@
       vm.event = eventsService.api(id).get(() => {
         vm.event.isToday = moment().isSame(vm.event.date, 'd');
       });
-    }
+    };
 
     vm.removeEvent = function(event) {
-      eventsService.api(event._id).remove(function() {
-        vm.getEvents();
+      dialogService.confirm('Are you sure you want to delete this event?').then(() => {
+        eventsService.api(event._id).remove(function() {
+          $state.go('events.list');
+        });
       });
-    }
+    };
 
     vm.newGame = function() {
       vm.game = {
         event: vm.event,
         number: vm.event.games.length + 1
-      }
+      };
       gamesService.api().create(vm.game, function(game) {
         vm.viewGame(game);
       }, function(err) {
@@ -45,26 +46,28 @@
         $state.transitionTo('events.list');
       }
 
-    }
+    };
 
     vm.viewGame = function(game) {
       $state.transitionTo('games.view', {
         id: game._id
-      })
-    }
+      });
+    };
 
     vm.completeGame = function(game) {
-      var message = `Game will be marked complete and scores cannot be changed.
-      Scores will be submitted to season standings.`
+      var message =
+        `Game will be marked complete and scores cannot be changed.
+      Scores will be submitted to season standings.`;
+
       dialogService.confirm(message).then(() => {
         game.completed = true;
         gamesService.api().save(game);
-      })
-    }
+      });
+    };
 
     function initialize() {
       if ($stateParams.id) {
-        vm.getEvent($stateParams.id)
+        vm.getEvent($stateParams.id);
       }
     }
 
@@ -72,4 +75,4 @@
 
   }
 
-})(angular);
+}
