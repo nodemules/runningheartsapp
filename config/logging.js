8af04299d2902105c45d9c34bbf470a3f8b0e3d5
ProbableCohort <brenthaertlein@gmail.com>
2017-08-30 01:33:22 -0400

    hotfix/RHP_223-improve-logging
    - lets use log4js instead, for native linenumber support and https://npmcompare.com/compare/bristol,bunyan,log4js,loggin,scribe

--- config/logging.js
+++ config/logging.js
@@ -1,17 +1,19 @@
 {
   function exports() {
 
-    const winston = require('winston');
-    const winstonFormatter = require('winston-console-formatter');
+    const log4js = require('log4js');
+    const log4js_extend = require('log4js-extend');
+
+    log4js_extend(log4js, {
+      path: __dirname,
+      format: '| @name (@file:@line:@column)'
+    });
 
     const env = require('./environment');
 
     // define our custom logger
-    var logger = new winston.Logger({
-      level: env.getName() === 'dev' ? 'silly' : 'info'
-    });
-
-    logger.add(winston.transports.Console, winstonFormatter.config());
+    var logger = log4js.getLogger();
+    logger.level = env.getName() === 'dev' ? 'debug' : 'info';
 
     logger.info('Environment loaded: [%s]', env.getName());
     logger.info('Logging level is set to %s', logger.level);
