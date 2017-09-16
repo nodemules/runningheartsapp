485bf27e9c59a6990d19eda5661401614b5bfbee
brenthaertlein <brenthaertlein@gmail.com>
2017-09-16 09:44:21 -0400

    feature/RHP_224-gruntbabelminify
    - minify css
    - minify vendor js
    - miniify vendor css
    - enable gzip compression for text-based resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,22 @@
         build: {
           src: ['public/javascripts/**/*.js'],
           dest: 'src/<%= pkg.name %>.es6'
+        },
+        css: {
+          src: ['public/stylesheets/**/*.css'],
+          dest: 'src/<%= pkg.name %>.css'
+        }
+      },
+      bower_concat: {
+        build: {
+          dest: {
+            js: 'src/vendor.js',
+            css: 'src/vendor.css'
+          },
+          dependencies: {
+            'angular': 'jquery',
+            'angular-material': 'angular'
+          }
         }
       },
       browserify: {
@@ -29,19 +45,28 @@
         }
       },
       uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
-        },
         build: {
           src: 'src/<%= pkg.name %>.js',
           dest: 'public/dist/<%= pkg.name %>.min.js'
+        },
+        vendor: {
+          src: 'src/vendor.js',
+          dest: 'public/dist/vendor.min.js'
+        }
+      },
+      cssmin: {
+        build: {
+          files: {
+            'public/dist/<%= pkg.name %>.min.css': ['src/<%= pkg.name %>.css'],
+            'public/dist/vendor.min.css': ['src/vendor.css']
+          }
         }
       },
       nodemon: {
         dev: {
           script: 'server.js',
           options: {
-            ignore: ['src', 'public/javascripts']
+            ignore: ['src', 'public/javascripts', 'Gruntfile.js']
           }
         }
       },
@@ -51,6 +76,7 @@
       },
       concurrent: {
         dev: ['nodemon:dev', 'watch'],
+        minify: ['uglify:build', 'uglify:vendor', 'cssmin:build'],
         options: {
           logConcurrentOutput: true
         }
@@ -59,13 +85,18 @@
 
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('develop', ['concat:build', 'browserify:dist', 'uglify:build', 'concurrent:dev']);
+    grunt.registerTask('develop', ['concat:build', 'concat:css', 'bower_concat:build', 'browserify:dist',
+      'concurrent:minify',
+      'concurrent:dev'
+    ]);
 
   };
 }
